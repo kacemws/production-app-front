562caf413e6124b3a1bab5804402615f041e0b88
kacemws <berras.belkacem@gmail.com>
2021-06-16 00:01:37 +0100

    updating loading screens

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import AccessibleNavigationAnnouncer from "./components/AccessibleNavigationAnnouncer";
 import Cookies from "js-cookie";
 import { refreshExpiredToken, setAuthToken } from "./api/axios";
+import ThemedSuspense from "./components/ThemedSuspense";
 
 const Layout = lazy(() => import("./containers/Layout"));
 const Login = lazy(() => import("./pages/Login"));
@@ -112,10 +113,14 @@ function App() {
   }
   return (
     <>
-      <Router>
-        <AccessibleNavigationAnnouncer />
-        <Switch>{routes}</Switch>
-      </Router>
+      {fetchingToken ? (
+        <ThemedSuspense />
+      ) : (
+        <Router>
+          <AccessibleNavigationAnnouncer />
+          <Switch>{routes}</Switch>
+        </Router>
+      )}
     </>
   );
 }
