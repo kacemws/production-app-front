7f4239d8c5e2e6c2f57ee44a02b26b238999e088
kacemws <berras.belkacem@gmail.com>
2021-06-15 15:02:26 +0100

    changed app name

--- src/components/Sidebar/SidebarContent.js
+++ src/components/Sidebar/SidebarContent.js
@@ -17,7 +17,7 @@ function SidebarContent() {
         className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
         href="#"
       >
-        Windmill
+        PM
       </a>
       <ul className="mt-6">
         {routes.map((route) =>
